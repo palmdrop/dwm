38d291a70945ff04344c676c4e94d613d8198794
palmdrop <anton@exlex.se>
2020-09-19 13:30:36 +0200

    testing

--- config.def.h
+++ config.def.h
@@ -147,8 +147,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-F", NULL };
-static const char *dmenucmd[] = { "dmenu_run_dunst", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-F", NULL };
+//static const char *dmenucmd[] = { "dmenu_run_dunst", NULL };
 static const char *wificmd[] = { "networkmanager_dmenu", "-m", dmenumon, "-i", "-F", NULL };
 
 static const char *termcmd[]         = { TERM, "-g", TERMSIZE, NULL };
