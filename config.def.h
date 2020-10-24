63bd2896977321ecd98d6032af1730ecb4cdff84
palmdrop <anton@exlex.se>
2020-10-25 01:02:49 +0200

    inset toggle not necessary atm

--- config.def.h
+++ config.def.h
@@ -214,7 +214,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("togglebardwm") },
-	{ MODKEY|ShiftMask,             XK_b,      toggleinset,    {0} },
+	//{ MODKEY|ShiftMask,             XK_b,      toggleinset,    {0} },
 
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
